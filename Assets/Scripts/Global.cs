b5b5764c9a2037487753a45608c456a044b31138
Scodobor <codobor@mail.ru>
2025-04-21 14:17:23 +0300

    v1.2
    Поменял базу данных

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -8,9 +8,9 @@ public static class Global
 
     public static MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
     {
-        Server = "sql7.freesqldatabase.com",
-        UserID = "sql7758157",
-        Password = "W8e5PQgmU4",
-        Database = "sql7758157",
+        Server = "sql.freedb.tech",
+        UserID = "freedb_antonov",
+        Password = "P*zwcAWSc7&h@5E",
+        Database = "freedb_mgkeit",
     };
 }
